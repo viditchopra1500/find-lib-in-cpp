5033a641f9b9d8a0af2093af0be92a1ec0dcc8b8
viditchopra1500 <vidit1500@gmail.com>
2020-05-25 22:40:43 +0530

    made dummy lib for testing

---
+++ lib/game.cpp
@@ -0,0 +1,9 @@
+#include "game.h"
+#include <stdio.h> 
+#include <dirent.h> 
+#include<bits/stdc++.h>
+using namespace std;
+void foo( int add)
+{
+	cout<<add;
+}
---
+++ lib/game.h
@@ -0,0 +1,9 @@
+#ifndef WHATEVER_H_INCLUDED
+#define WHATEVER_H_INCLUDED
+#include <stdio.h> 
+#include <dirent.h> 
+#include<bits/stdc++.h>
+using namespace std;
+#define WHATEVER_H_INCLUDED
+    void foo(int add);
+#endif
